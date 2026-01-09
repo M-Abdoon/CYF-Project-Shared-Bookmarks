fc0e622d6842475a881d433afaec64a05dcbee35
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-01-09 07:48:39 +0000

    Project all completed

--- bookmarks/script.js
+++ bookmarks/script.js
@@ -11,6 +11,7 @@ import { clearData } from "./storage.js";
 const selectIdDropDown = document.getElementById("selectId");
 const dataList = document.getElementById("dataList");
 const stringListOfbookamrs = document.getElementById("stringListOfbookamrs");
+const clearAllData = document.getElementById("clearAllData");
 
 const addBookmarkForm = document.getElementById("addBookmarkForm");
 const bookmarkTitleInput = document.getElementById("bookmarkTitleInput");
@@ -20,14 +21,7 @@ const bookmarkLinkInput = document.getElementById("bookmarkLinkInput");
 const users = getUserIds();
 let selectedId = 1; // default
 
-const newData = [
-	{ title: "here", description: "hello this is me!", link: "https://google.com", timestamp: Date.now() },
-	{ title: "here2", description: "hello this is me!2", link: "https://facebookc.com", timestamp: Date.now() }
-];
-setData("1", newData);
-
 function setup () {
-	
 	
 	fillDropdown(users);
 	
@@ -40,9 +34,9 @@ function setup () {
 
 		if (getData(selectedId) == null || getData(selectedId).length < 1) {
 			stringListOfbookamrs.textContent = "No bookmarks for this user";
-
 			return false;
 		}
+
 		showDataList(selectedId);
 	});
 	
@@ -50,27 +44,43 @@ function setup () {
 		event.preventDefault();
 
 		const enteredTitle = bookmarkTitleInput.value;
-		const enteredDescription = bookmarkDescriptionInput.value;
 		const enteredLink = bookmarkLinkInput.value;
+		const enteredDescription = bookmarkDescriptionInput.value;
 
-		let getUserBookmark = getData(selectedId) ?? [];
-
-		const newEntryJson = { 
-			title: enteredTitle, 
-			description: enteredDescription, 
-			link: enteredLink, 
-			timestamp: Date.now()
-		};
-
-		getUserBookmark.push(newEntryJson);
-
-		setData(selectedId, getUserBookmark);
+		enterData(enteredTitle, enteredLink, enteredDescription);
 
 		render ();
+
 		showDataList(selectedId);
 
 	});
+
+	clearAllData.addEventListener("click", () => {
+		clearAllDataFunc();
+		render();
+	});
 };
+
+function enterData (enteredTitle, enteredLink, enteredDescription) {
+
+	if( enteredTitle == "" || enteredLink == "" || enteredDescription == "") {
+		alert("all fields must be filled");
+		return false;
+	}
+
+	let getUserBookmark = getData(selectedId) ?? [];
+	
+	const newEntryJson = { 
+		title: enteredTitle, 
+		link: enteredLink, 
+		description: enteredDescription, 
+		timestamp: Date.now()
+	};
+
+	getUserBookmark.push(newEntryJson);
+
+	setData(selectedId, getUserBookmark);
+}
 
 function fillDropdown (userIds) {
 	userIds.forEach(( id ) => {
@@ -79,27 +89,36 @@ function fillDropdown (userIds) {
 		createOption.text = "User Id Number " + id;
 		selectIdDropDown.appendChild(createOption);
 	});
-}
+};
 
 function render() {
 	dataList.innerHTML = "";
 	stringListOfbookamrs.innerHTML = "";
-}
-function showDataList(dataOfUser) {
+};
+
+function showDataList(userId) {
+
+	const dataOfUser = getData(userId);
+
+	if(dataOfUser == null || dataOfUser.length == 0)
+		return false;
 	
-	dataOfUser = getData(dataOfUser);
 	dataOfUser.sort((a, b) => b.timestamp - a.timestamp);
 	dataOfUser.forEach(( item ) => {
 		
 		const p = document.createElement("p");
 		const a = document.createElement("a");
+		const font = document.createElement("font");
 		const desc = document.createElement("p");
 		const timestamp = document.createElement("p");
 		const hr = document.createElement("hr");
 		
 		a.href = item.link;
-		a.textContent = item.title;
 		
+		font.textContent = item.title;
+		font.color = "black";
+
+		a.appendChild(font);
 		p.appendChild(a);
 		
 		desc.textContent = item.description;
@@ -113,5 +132,11 @@ function showDataList(dataOfUser) {
 		
 	});
 };
+
+function clearAllDataFunc() {
+	getUserIds().forEach((userId) => {
+		clearData(userId);
+	});
+}
 
 window.onload = setup();
